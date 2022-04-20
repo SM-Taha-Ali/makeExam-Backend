fc1828e61a4a232e44b4d6391c7ccf32d642e50d
unknown <sskk8226@gmail.com>
2022-04-20 16:25:17 +0500

    done

--- controllers/pastpapersKB.js
+++ controllers/pastpapersKB.js
@@ -1,57 +1,31 @@
-const express = require('express');
-const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const PastpapersKB = require('../models/PastpapersKB.js')
 
-const User = require('../models/Users')
-
-const JWT_SECRET = "clix123#p$rO"
 
 async function createPaper(req, res) {
     // Check whether the user with this email already exists
 
     let success = false
     try {
-        const errors = validationResult(req);
-        // Checking if validations are fulfilled
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ success, errors: errors.array() });
-        }
-        let user = await User.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(400).json({ success, error: "Sorry a user with this email already exists." })
-        }
-        const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt);
         // Creating user by using create method of mongoose model
-        user = await User.create({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            password: secPass,
-            email: req.body.email,
+        let paper = await PastpapersKB.create({
+            year: req.body.year,
+            section: req.body.section,
+            marks: req.body.marks,
+            question: req.body.question,
+            subParts: req.body.subParts,
         });
-        // Sending the user object as a response
-        const data = {
-            user: {
-                id: user.id
-            }
-        }
-        const authToken = jwt.sign(data, JWT_SECRET);
-        console.log(authToken);
-        success = true
-        res.json({ success, authToken });
+        res.json(paper);
     }
     catch (error) {
         console.log(error);
         res.status(500).send(success, "Internal server error")
     }
-
 }
 
 async function getPaper(req, res) {
     try {
-        const users = await User.find().select("-password");
-        res.json(users);
+        const paper = await PastpapersKB.find();
+        res.json(paper);
     }
     catch (error) {
         console.log(error);
@@ -59,22 +33,22 @@ async function getPaper(req, res) {
     }
 }
 
-async function updateRole_Status(req, res) {
-    try {
-        let item = await User.findById(req.body.id);
-        if (!item) { return res.status(404).send("Not found!") }
-        const { status } = req.body;
-        const { role } = req.body;
-        const newItem = {};
-        if ("boolean" === typeof(status)) { newItem.status = status }
-        if (role) { newItem.role = role }
-        item = await User.findByIdAndUpdate(req.body.id, { $set: newItem }, { new: true })
-        res.json(newItem);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal server error")
-    }
-}
+// async function updateRole_Status(req, res) {
+//     try {
+//         let item = await User.findById(req.body.id);
+//         if (!item) { return res.status(404).send("Not found!") }
+//         const { status } = req.body;
+//         const { role } = req.body;
+//         const newItem = {};
+//         if ("boolean" === typeof(status)) { newItem.status = status }
+//         if (role) { newItem.role = role }
+//         item = await User.findByIdAndUpdate(req.body.id, { $set: newItem }, { new: true })
+//         res.json(newItem);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send("Internal server error")
+//     }
+// }
 
 
 module.exports = {
--- routes/pastpapersKB.js
+++ routes/pastpapersKB.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const { body } = require('express-validator');
 const router = express.Router();
 
 // Importing controller
 
 const authController = require('../controllers/pastpapersKB.js');
-const fetchuser = require('../middlewares/fetchUser');
 
 // Creating User
 
